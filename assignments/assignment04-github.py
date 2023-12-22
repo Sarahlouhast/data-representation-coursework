60aafcc1533f3fce027a55cec579dcb0b0bc911e
Sarah Hastings <G00235562@atu.ie>
2023-12-22 21:07:02 +0000

    updates

--- assignments/assignment04-github.py
+++ assignments/assignment04-github.py
@@ -2,6 +2,7 @@
 # Task: Write a program in python that will read a file from a repository. The program should then replace all the instances of the text "Andrew" with your name. The program should then commit those changes and push the file back to the repository. (https://vlegalwaymayo.atu.ie/mod/page/view.php?id=775932)
 # Author: Sarah Hastings
 
+
 # Import libraries and config file with api keys for access to github
 from github import Github
 import requests
