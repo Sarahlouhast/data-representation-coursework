321d9a6b6ea7993c691a49359a4b435aa90c5b1c
Sarah Hastings <G00235562@atu.ie>
2023-12-22 18:16:23 +0000

    Work on assignment 4 changes

--- assignments/assignment04-github.py
+++ assignments/assignment04-github.py
@@ -1,15 +1,53 @@
-#Write a program in python that will read a file from a repository, 
-#The program should then replace all the instances of the text "Andrew" with your name
-#The program should then commit those changes and push the file back to the repository.
-#I do not need to see your keys (see lab2, to follow)
-#Handup: Save the program as assignment04-github.py to the same repository you uploaded the xml to
-#Marks: Marks will be given for the functionality and layout of the code; I do expect minimal comments to indicate you know what the program is doing
-ss
+# Assignment 04 - assignment04-github.py
+# Task: Write a program in python that will read a file from a repository. The program should then replace all the instances of the text "Andrew" with your name. The program should then commit those changes and push the file back to the repository. (https://vlegalwaymayo.atu.ie/mod/page/view.php?id=775932)
+# Author: Sarah Hastings
 
-d
-x
-t
+# Import libraries and config file with api keys for access to github
+from github import Github
+import requests
+import re # case-insensitive replacement
+from config import config as cfg
 
+# Function to update a file in a GitHub repository
+def update_github_file(repo_name, file_name, old_text, new_text, github_api_key):
+    try:
+        # Authenticate with GitHub using the provided API key
+        repo = Github(github_api_key).get_repo(repo_name)
 
+        # Get information about the file to be updated
+        file_info = repo.get_contents(file_name)
 
+        # Make a HTTP request to download the file content
+        content_of_file = requests.get(file_info.download_url).text
+        
+        # Replace instances of the old text with the new text. Use regular expressions (re) for case-insensitive replacement
+        new_contents = re.sub(re.escape(old_text), new_text, content_of_file, flags=re.IGNORECASE)
 
+        # Note if you do want to replace instances of the old text with the new text without case-sensitivity use the below
+        #new_contents = content_of_file.replace(old_text, new_text)
+
+        # Check if any changes were made
+        if content_of_file == new_contents:
+            print(f"No changes to make in {file_info.path}")
+            return
+
+        # Commit the changes to the file in the repository
+        commit_message = f"Replace '{old_text}' with '{new_text}'"
+        repo.update_file(file_info.path, commit_message, new_contents, file_info.sha)
+
+        # Print a message indicating the success of the operation
+        print(f"Changes committed and pushed to {file_info.path}")
+
+    # Handle errors
+    except (requests.exceptions.RequestException, Exception) as e:
+        print(f"An error occurred: {e}")
+
+# Example usage
+repo_name = "Sarahlouhast/aprivateone"
+file_name = "test.txt"
+old_name = "Andrew"
+new_name = "Sarah"
+api_key = cfg["githubkey"]
+
+# Call the function with the specified parameters
+update_github_file(repo_name, file_name, old_name, new_name, api_key)
---
+++ assignments/config.py
@@ -0,0 +1,6 @@
+# Config files
+
+config = {
+    "htmltopdfkey":"ClPY9iEAkf3QBJeHG7aQ46c5C1Ss4LuHEfRDZKYNMW4xtQxlNWAlqoyh1zaTuhqb",
+    "githubkey":"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"
+}
